ed862c36b0e55f69237d76a4e3cb0db23c18c216
Justin Toupin <justin@atendesigngroup.com>
2021-07-07 14:16:45 -0600

    Expand / fix access check to include paragraph types and cover create/update/delete operations.

--- src/Access/LayoutParagraphsBuilderAccess.php
+++ src/Access/LayoutParagraphsBuilderAccess.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\layout_paragraphs\Access;
 
-use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\paragraphs\ParagraphsTypeInterface;
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\layout_paragraphs\LayoutParagraphsLayout;
 
 /**
@@ -14,25 +16,70 @@ use Drupal\layout_paragraphs\LayoutParagraphsLayout;
 class LayoutParagraphsBuilderAccess implements AccessInterface {
 
   /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Class constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
    * {@inheritDoc}
    *
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The account to check access for.
    * @param \Drupal\layout_paragraphs\LayoutParagraphsLayout $layout_paragraphs_layout
    *   The layout paragraphs layout object.
+   * @param string $operation
+   *   The operation being peformed (i.e. 'create' or 'delete').
+   * @param string $component_uuid
+   *   The specific component being acted on.
+   * @param \Drupal\paragraphs\ParagraphsTypeInterface $paragraph_type
+   *   The paragraph type of a component being added.
    *
    * @return \Drupal\Core\Access\AccessResult
    *   The access result.
    */
-  public function access(AccountInterface $account, LayoutParagraphsLayout $layout_paragraphs_layout) {
+  public function access(
+    AccountInterface $account,
+    LayoutParagraphsLayout $layout_paragraphs_layout,
+    string $operation = NULL,
+    string $component_uuid = NULL,
+    ParagraphsTypeInterface $paragraph_type = NULL
+  ) {
+
+    // Check field access.
     $entity = $layout_paragraphs_layout->getEntity();
-    $field_access = $layout_paragraphs_layout->getParagraphsReferenceField()->access('edit', $account, TRUE);
-    if ($entity->isNew()) {
-      return $entity->access('create', $account, TRUE)->andIf($field_access);
+    $access = $layout_paragraphs_layout->getParagraphsReferenceField()->access('edit', $account, TRUE);
+
+    // Check access to host entity.
+    $lp_operation = $entity->isNew() ? 'create' : 'update';
+    $access = $access->andIf($entity->access($lp_operation, $account, TRUE));
+
+    // Check access to specific paragraph entity.
+    if ($component_uuid) {
+      $paragraph = $layout_paragraphs_layout
+        ->getComponentByUuid($component_uuid)
+        ->getEntity();
+      $access = $access->andIf($paragraph->access($operation, $account, TRUE));
     }
-    else {
-      return $entity->access('update', $account, TRUE)->andIf($field_access);
+
+    // Check access to paragraph type.
+    if ($paragraph_type) {
+      $access_handler = $this->entityTypeManager->getAccessControlHandler('paragraph');
+      $access = $access->andIf($access_handler->createAccess($paragraph_type->id(), $account, [], TRUE));
     }
+
+    return $access;
   }
 
 }
