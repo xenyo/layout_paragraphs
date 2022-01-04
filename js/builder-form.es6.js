b114354527fa4220dd84b8297b815284d3b5afd6
Justin Toupin <justin@atendesigngroup.com>
2022-01-04 06:37:19 -0700

    Added es6.js files

---
+++ js/builder-form.es6.js
@@ -0,0 +1,21 @@
+(($, Drupal) => {
+  // Updates the "Close" button label when a layout is changed.
+  Drupal.behaviors.layoutParagraphsBuilderForm = {
+    attach: function attach(context) {
+      const events = [
+        'lpb-component:insert.lpb',
+        'lpb-component:update.lpb',
+        'lpb-component:move.lpb',
+        'lpb-component:drop.lpb',
+      ].join(' ');
+      $('[data-lpb-id]', context)
+        .once('lpb-builder-form')
+        .on(events, (e) => {
+          $(e.currentTarget)
+            .closest('[data-lpb-form-id]')
+            .find('[data-drupal-selector="edit-close"]')
+            .val(Drupal.t('Cancel'));
+        });
+    },
+  };
+})(jQuery, Drupal);
