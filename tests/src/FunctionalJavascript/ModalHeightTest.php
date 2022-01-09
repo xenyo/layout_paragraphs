b82897c40c96d85b16d251a998059bbd36504158
Justin Toupin <justin@atendesigngroup.com>
2022-01-08 17:51:15 -0700

    wip - added height test

--- tests/src/FunctionalJavascript/ModalHeightTest.php
+++ tests/src/FunctionalJavascript/ModalHeightTest.php
@@ -2,41 +2,126 @@
 
 namespace Drupal\Tests\layout_paragraphs\FunctionalJavascript;
 
+use Drupal\editor\Entity\Editor;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\filter\Entity\FilterFormat;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
+
 /**
  * Tests that buttons remain reachable even with tall modal heights.
+ *
+ * @requires module layout_paragraphs_alter_controls_test
  *
  * @group layout_paragraphs
  */
 class ModalHeightTest extends BuilderTestBase {
 
   /**
-   * Tests that buttons remain reachable even with tall modal heights.
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'layout_paragraphs',
+    'paragraphs',
+    'node',
+    'field',
+    'field_ui',
+    'block',
+    'paragraphs_test',
+    'ckeditor',
+    'filter',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'seven';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+
+    parent::setUp();
+    $this->addParagraphsType('rich_text');
+
+    // Create a text format and associate CKEditor.
+    $this->filterFormat = FilterFormat::create([
+      'format' => 'filtered_html',
+      'name' => 'Filtered HTML',
+      'weight' => 0,
+    ]);
+    $this->filterFormat->save();
+
+    Editor::create([
+      'format' => 'filtered_html',
+      'editor' => 'ckeditor',
+    ])->save();
+
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => 'field_rich_text',
+      'type' => 'text_long',
+      'entity_type' => 'paragraph',
+    ]);
+    $field_storage->save();
+
+    // Create a body field instance for the 'page' node type.
+    FieldConfig::create([
+      'field_storage' => $field_storage,
+      'bundle' => 'rich_text',
+      'label' => 'Body',
+      'settings' => ['display_summary' => TRUE],
+      'required' => TRUE,
+    ])->save();
+
+    // Assign widget settings for the 'default' form mode.
+    EntityFormDisplay::create([
+      'targetEntityType' => 'paragraph',
+      'bundle' => 'rich_text',
+      'mode' => 'default',
+      'status' => TRUE,
+    ])->setComponent('field_rich_text', ['type' => 'text_textarea'])
+      ->save();
+  }
+
+  /**
+   * Tests modal height.
    */
   public function testModalHeight() {
     $this->loginWithPermissions([
       'create page content',
       'edit own page content',
+      'use text format filtered_html',
     ]);
+
     $this->drupalGet('node/add/page');
     $page = $this->getSession()->getPage();
 
-    // Click the Add Component button.
-    $page->find('css', '.lpb-btn--add')->click();
-    $this->assertSession()->assertWaitOnAjaxRequest(1000, 'Unable to click add a component.');
+    $button = $page->find('css', '.lpb-btn--add');
+    $button->click();
+    $this->assertSession()->assertWaitOnAjaxRequest();
 
-    // Open the "Create text" dialog.
-    $page->clickLink('text');
-    $this->assertSession()->waitForText('Create text');
+    $button = $page->find('css', '.type-rich_text');
+    $button->click();
+    $this->assertJsCondition('typeof CKEDITOR !== "undefined";');
+    $this->assertJsCondition('Object.keys(CKEDITOR.instances).length > 0;');
+    $this->htmlOutput();
+    // Causes the dialog height to grow beyond the viewport.
+    $text = "-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />-- rich text line --<p />";
+    $this->getSession()->executeScript(<<<JS
+jQuery('.layout-paragraphs-component-form').height('1000px');
+JS);
+    $button = $page->find('css', '.ui-dialog-buttonpane .lpb-btn--save');
+    $button->click();
 
-    // Force the dialog height to expand beyond the viewport.
-    // Since the dialog height is 'auto' by default, increasing the height of
-    // the dialog's content will automatically increase the dialog's height.
-    $this->getSession()->executeScript('jQuery(\'.layout-paragraphs-component-form\').height(\'2000px\');');
-    // Pause for UI to update.
-    $this->getSession()->wait(1000);
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    //$this->assertSession()->pageTextContains('-- rich text line --');
+    $this->htmlOutput();
 
-    // Save button should still be reachable.
-    $this->assertSession()->assertVisibleInViewport('css', '.ui-dialog-buttonpane .lpb-btn--save');
+    // $this->submitForm([
+    //   'title[0][value]' => 'Node title',
+    // ], 'Save');
+    //$this->assertSession()->pageTextContains('-- rich text line --');
 
   }
 
